427f54868b3f37e96b2c5875721d277b26fd952d
Amrut <XKS9@eurofins.com>
2019-07-30 12:35:36 +0530

    Disabling Auth

--- src/main/java/com/dashlines/gateway/config/AppSecurityConfig.java
+++ src/main/java/com/dashlines/gateway/config/AppSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.dashlines.gateway.config;
+/*package com.dashlines.gateway.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -19,4 +19,4 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 	}
-}
+}*/
